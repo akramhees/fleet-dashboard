4eade8be8c9893035738ca0619bda4a60a14d035
akramhees <akramh2454@gmail.com>
2026-06-29 11:53:26 -0600

    Fixed API calls to use localhost

--- client/src/Login.js
+++ client/src/Login.js
@@ -15,7 +15,7 @@ function Login({ onLogin, onSignUp }) {
     setMessage('');
 
     try {
-      const response = await fetch('${process.env.REACT_APP_API_URL}/api/login', {
+      const response = await fetch('http://localhost:5001/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -36,7 +36,7 @@ function SignUp({ onBackToLogin }) {
     }
 
     try {
-      const response = await fetch('${process.env.REACT_APP_API_URL}/api/signup', {
+      const response = await fetch('http://localhost:5001/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
